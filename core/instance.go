d50abc41acae416118fb5ac30e2e2fa627085572
Justin SB <justin@fathomdb.com>
2014-04-16 02:50:00 -0700

    Hack to wait for properties to be set

--- core/instance.go
+++ core/instance.go
@@ -100,6 +100,35 @@ func (self *Instance) GetState() (*model.Instance, error) {
 		}
 	}
 
+	// TODO: This is a bit of a hack also.  How should we wait for properties to be set?
+	annotations, err := client.GetServiceAnnotations(primaryServiceId)
+	if err != nil {
+		log.Warn("Error getting annotations", err)
+		// TODO: Mask error?
+		return nil, err
+	}
+
+	log.Info("Annotations on %v: %v", primaryServiceId, annotations)
+
+	// First call
+	//__jxaas_relinfo_0_db:46__private-address:10.0.3.176 __jxaas_relinfo_0_db:46_timestamp:50]
+
+	// Ready call
+	//__jxaas_relinfo_0_db:46__database:u2c1f1c9f92d7481a8015fd6b53fb2f26-mysql-jk-proxy-proxyclient __jxaas_relinfo_0_db:46__host:10.0.3.176 __jxaas_relinfo_0_db:46__password:oozahghaicongoo __jxaas_relinfo_0_db:46__private-address:10.0.3.176 __jxaas_relinfo_0_db:46__slave:False __jxaas_relinfo_0_db:46__user:cahshaimesaecae __jxaas_relinfo_0_db:46_timestamp:0]
+
+	// TODO: This is a total hack... need to figure out when annotations are 'ready' and when not.
+	// we probably should do this on set, either in the charms or in the SetAnnotations call
+	annotationsReady := false
+	for key, _ := range annotations {
+		if strings.HasSuffix(key, "__password") {
+			annotationsReady = true
+		}
+	}
+
+	if !annotationsReady {
+		instance.Status = "pending"
+	}
+
 	return instance, nil
 }
 
