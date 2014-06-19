c9be24ae26972233cb94f3e0d7b1fe1a148e1624
Justin SB <justin@fathomdb.com>
2014-06-19 00:58:59 -0700

    Fix build.sh, to work around godeps bzr branch issue
    It looks like godeps can't pull code, and can't switch branches in bzr.
    
    Instead, we pull the code first, and then we run godeps.

--- build.sh
+++ build.sh
@@ -12,11 +12,25 @@ if [ ! -h ${GOPATH}/src/github.com/jxaas/jxaas ]; then
 	ln -s ../../../../.. ${GOPATH}/src/github.com/jxaas/jxaas
 fi
 
+
+# Get the trusty version of juju-core
+# It would sure be nice if godeps handled go dependencies...
+if [ ! -d ${GOPATH}/src/launchpad.net/juju-core ]; then
+       mkdir -p ${GOPATH}/src/launchpad.net
+       echo "Fetching lp:juju-core/1.18"
+       pushd ${GOPATH}/src/launchpad.net/
+       bzr branch lp:juju-core/1.18 juju-core
+       popd
+fi
+
 # Get the godeps tool
 go get launchpad.net/godeps
 
 # Download, but do not install, the latest code (to seed godeps)
 go get -d -u -v github.com/jxaas/jxaas
+
+# Install some dependencies (these are otherwise missed?)
+go get -d github.com/mattbaird/elastigo
 
 # Install the correct versions of dependencies
 ${GOBIN}/godeps -u dependencies.tsv 
