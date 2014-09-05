e5369a51e809b0d16ac6033b20160777f2b86ad1
Justin SB <justin@fathomdb.com>
2014-09-04 21:33:50 -0700

    Support basic keystone auth
    We use the credential pass-through technique, for simplicity,
    and because this works until federation is implemented & supported.

--- endpoints/root.go
+++ endpoints/root.go
@@ -4,7 +4,10 @@ import (
 	"encoding/base64"
 	"net/http"
 	"strings"
+
+	"github.com/justinsb/gova/log"
 	"github.com/justinsb/gova/rs"
+	"launchpad.net/goose/identity"
 )
 
 type EndpointXaas struct {
@@ -13,7 +16,9 @@ type EndpointXaas struct {
 func (self *EndpointXaas) Item(key string, req *http.Request) (*EndpointTenant, error) {
 	child := &EndpointTenant{}
 
-	var authorization *string
+	tenantName := strings.Replace(key, "-", "", -1)
+
+	var authorization *identity.AuthDetails
 
 	authorizationHeaders := req.Header["Authorization"]
 	if len(authorizationHeaders) > 0 {
@@ -28,22 +33,34 @@ func (self *EndpointXaas) Item(key string, req *http.Request) (*EndpointTenant, 
 				username := usernameAndPassword[0]
 				password := usernameAndPassword[1]
 
-				if username == password {
-					authorization = &username
+				log.Debug("Request to authenticate as: %v in tenant %v", username, tenantName)
+
+				// XXX: Don't hard-code
+				authServerUrl := "http://127.0.0.1:5000/v2.0"
+
+				authenticator := identity.NewAuthenticator(identity.AuthUserPass, nil)
+				creds := identity.Credentials{TenantName: tenantName, User: username, URL: authServerUrl + "/tokens", Secrets: password}
+				auth, err := authenticator.Auth(&creds)
+				if err != nil {
+					log.Warn("Error authenticating against Openstack Identity", err)
+				} else if auth == nil {
+					log.Warn("Auth returned from Openstack identity was nil")
+				} else {
+					log.Debug("Got auth token: %v", auth.TenantId)
+					authorization = auth
 				}
 			}
 		}
 	}
 
-	tenant := strings.Replace(key, "-", "", -1)
-
-	if authorization == nil || *authorization != tenant {
+	if authorization == nil {
 		notAuthorized := rs.HttpError(http.StatusUnauthorized)
 		notAuthorized.Headers["WWW-Authenticate"] = "Basic realm=\"jxaas\""
 		return nil, notAuthorized
 	}
 
-	child.Tenant = tenant
+	child.Tenant = tenantName
+	// TODO: Use tenantId? authorization.TenantId
 
 	return child, nil
 }
