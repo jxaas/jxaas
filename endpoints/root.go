905c669c0ea9a49544ee8c87ef66ab61a6b103e0
Justin SB <justin@fathomdb.com>
2014-09-10 07:40:54 -0700

    Authentication with openstack using token auth

--- endpoints/root.go
+++ endpoints/root.go
@@ -13,41 +13,82 @@ import (
 type EndpointXaas struct {
 }
 
+type Authorization struct {
+	TenantId   string
+	TenantName string
+}
+
 func (self *EndpointXaas) Item(key string, req *http.Request) (*EndpointTenant, error) {
 	child := &EndpointTenant{}
 
+	// XXX: Don't hard-code
+	authServerUrl := "http://127.0.0.1:5000/v2.0"
+	if !strings.HasSuffix(authServerUrl, "/") {
+		authServerUrl += "/"
+	}
+
+	tenantId := key
 	tenantName := strings.Replace(key, "-", "", -1)
 
-	var authorization *identity.AuthDetails
+	var authorization *Authorization
 
-	authorizationHeaders := req.Header["Authorization"]
-	if len(authorizationHeaders) > 0 {
-		authorizationHeader := strings.TrimSpace(authorizationHeaders[0])
+	authTokens := req.Header["X-Auth-Token"]
+	if len(authTokens) > 0 {
+		authToken := strings.TrimSpace(authTokens[0])
 
-		tokens := strings.SplitN(authorizationHeader, " ", 2)
-		if len(tokens) == 2 && tokens[0] == "Basic" {
-			payload, _ := base64.StdEncoding.DecodeString(tokens[1])
-			usernameAndPassword := strings.SplitN(string(payload), ":", 2)
+		log.Debug("Request to authenticate with token: %v in tenant: %v", authToken, tenantId)
 
-			if len(usernameAndPassword) == 2 {
-				username := usernameAndPassword[0]
-				password := usernameAndPassword[1]
+		tenants, err := identity.ListTenantsForToken(authServerUrl+"tenants", authToken, nil)
+		if err != nil {
+			log.Warn("Error authenticating against Openstack Identity", err)
+		} else if tenants == nil {
+			log.Warn("Tenants returned from Openstack identity was nil")
+		} else {
+			for _, tenant := range tenants {
+				if tenant.Id == tenantId {
+					if !tenant.Enabled {
+						log.Warn("In project, but not enabled for project: %v", tenantId)
+						continue
+					}
+					authorization = &Authorization{TenantId: tenant.Id, TenantName: tenant.Name}
+					break
+				}
+			}
 
-				log.Debug("Request to authenticate as: %v in tenant %v", username, tenantName)
+			if authorization == nil {
+				log.Warn("Valid token, but not authorized for project: %v", tenantId)
+			}
+		}
+	}
 
-				// XXX: Don't hard-code
-				authServerUrl := "http://127.0.0.1:5000/v2.0"
+	if authorization == nil {
+		authorizationHeaders := req.Header["Authorization"]
+		if len(authorizationHeaders) > 0 {
+			authorizationHeader := strings.TrimSpace(authorizationHeaders[0])
 
-				authenticator := identity.NewAuthenticator(identity.AuthUserPass, nil)
-				creds := identity.Credentials{TenantName: tenantName, User: username, URL: authServerUrl + "/tokens", Secrets: password}
-				auth, err := authenticator.Auth(&creds)
-				if err != nil {
-					log.Warn("Error authenticating against Openstack Identity", err)
-				} else if auth == nil {
-					log.Warn("Auth returned from Openstack identity was nil")
-				} else {
-					log.Debug("Got auth token: %v", auth.TenantId)
-					authorization = auth
+			tokens := strings.SplitN(authorizationHeader, " ", 2)
+			if len(tokens) == 2 && tokens[0] == "Basic" {
+				payload, _ := base64.StdEncoding.DecodeString(tokens[1])
+				usernameAndPassword := strings.SplitN(string(payload), ":", 2)
+
+				if len(usernameAndPassword) == 2 {
+					username := usernameAndPassword[0]
+					password := usernameAndPassword[1]
+
+					log.Debug("Request to authenticate as: %v in tenant %v", username, tenantName)
+
+					authenticator := identity.NewAuthenticator(identity.AuthUserPass, nil)
+					creds := identity.Credentials{TenantId: tenantId, User: username, URL: authServerUrl + "tokens", Secrets: password}
+					auth, err := authenticator.Auth(&creds)
+					if err != nil {
+						log.Warn("Error authenticating against Openstack Identity", err)
+					} else if auth == nil {
+						log.Warn("Auth returned from Openstack identity was nil")
+					} else {
+						log.Debug("Got auth token: %v", auth.TenantId)
+						authorization = &Authorization{TenantId: auth.TenantId, TenantName: auth.TenantName}
+
+					}
 				}
 			}
 		}
