8603203e2ae065e1c15c709f1d596f562671c56f
Justin SB <justin@fathomdb.com>
2014-10-24 05:02:14 -0700

    Add TODO for auth on private API

--- endpoints/privateroot.go
+++ endpoints/privateroot.go
@@ -3,6 +3,9 @@ package endpoints
 type EndpointXaasPrivate struct {
 }
 
+// TODO: Add auth
+// (and then add to python lib)
+// (and then add to stubclient & maybe proxy)
 func (self *EndpointXaasPrivate) ItemRpc() *EndpointRpc {
 	child := &EndpointRpc{}
 	return child
